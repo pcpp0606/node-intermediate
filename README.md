31880c96d22c30c580bb45ecedf78551c69d4327
PARKGEUNGYUN <pcpp0606@naver.com>
2024-05-31 03:04:40 +0900

    save

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-node-intermediate
+# node-intermediate
+
+# 실행 방법
+
+- 필요한 패키지 설치
+
+```sh
+yarn
+```
+
+- 서버 실행 (배포용)
+```sh
+yarn start
+```
+
+- 서버 실행 (개발용)
+```sh
+yarn dev
+```
--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import express from "express";
+import { SERVER_PORT } from './constants/env.constant.js';
+import { errorHandler } from "./middlewares/error-handler.middleware.js";
+import { HTTP_STATUS } from "./constants/http-status.constant.js";
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('서버가 3000번 포트에서 실행 중입니다.');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/health-check', (req, res) => {
+    return res.status(HTTP_STATUS.OK).json(`I'm healthy.`);
+});
+
+app.use(errorHandler);
+
+app.listen(SERVER_PORT, () => {
+    console.log(`서버가 ${SERVER_PORT}번 포트에서 실행 중입니다.`);
 });
